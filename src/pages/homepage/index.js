5c54d24c8ab92bca4efd677bd448bbb9903f88df
SergPaderov <sergeipaderov@gmail.com>
2019-07-29 22:02:51 -0300

    styled menu items

---
+++ src/pages/homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styled.scss";
+import Directory from "../../components/directory";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
