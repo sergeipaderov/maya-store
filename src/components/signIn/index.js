a468c1b7818bcc98796ad6e9cb4606c63e948c48
SergPaderov <sergeipaderov@gmail.com>
2019-08-04 13:37:54 -0300

    added sign-in with email and password

--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import "./styled.scss";
 import FormInput from "../form-input";
 import CustomButton from "../customButton";
-import { signInWithGoogle } from "../../firebase/utils";
+import { auth, signInWithGoogle } from "../../firebase/utils";
 
 class SignIn extends Component {
   constructor(props) {
@@ -15,10 +15,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
