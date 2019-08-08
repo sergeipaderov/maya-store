e665e4fe6dcef708d4c75fb10587078b639cbcd3
SergPaderov <sergeipaderov@gmail.com>
2019-08-08 10:30:35 -0300

    shop data and cart reducer was  updated for implementation of quantity of products in the cart

--- src/components/customButton/index.js
+++ src/components/customButton/index.js
@@ -2,9 +2,17 @@ import React from "react";
 
 import "./styled.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    className={`
+      ${inverted ? "inverted" : ""} 
+      ${isGoogleSignIn ? "google-sign-in" : ""} custom-button
+    `}
     {...otherProps}
   >
     {children}
