e24d0d10bc67754396835ce049b571ce676f4790
SergPaderov <sergeipaderov@gmail.com>
2019-08-10 11:09:01 -0300

    using reselect selectors for cart-items

--- src/components/cartDropdown/index.js
+++ src/components/cartDropdown/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import CustomButton from "../customButton";
 import CartItem from "../cartItem";
+import { selectCartItems } from "../../redux/cart/cartSelectors";
 import "./styled.scss";
 
 const CartDropdown = ({ cartItems }) => (
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
